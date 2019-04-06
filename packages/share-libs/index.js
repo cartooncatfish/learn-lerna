b2c9324eada8a1337978f75ed195c7f0c95767d9
mikemo <cartooncatfish@gmail.com>
2019-04-06 23:54:11 +1100

    tweaks for new version

--- packages/share-libs/index.js
+++ packages/share-libs/index.js
@@ -11,7 +11,7 @@ export const myRandomNoGenerator = (min, max) => {
 	return Math.random() * (max - min) + min;
 };
 
-export const FixedData3 = {
+export const FixedData4 = {
 	f1: 'name',
 	f2: 'age',
 };
